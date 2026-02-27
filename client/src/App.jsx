c1492d44bd151aa7336a1c99f2e486a956beed7c
MercyPalleti <palletimercy1@gmail.com>
2026-02-27 23:30:45 +0530

    implemented route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Sermons from "./pages/Sermons";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-     <>
+    <div className="min-h-screen bg-gray-50">
       <Navbar />
-       <Home />
-    </>
-  )
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sermons" element={<Sermons />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
